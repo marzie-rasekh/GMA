b87bb93d7573149cb99e04a74c070613d7845b37
Zee <zee@marzie.bu.edu>
2017-07-10 18:18:00 -0400

    fixed lookatbadTRs and output format

--- postprocessing/lookAtBadTRs.sh
+++ postprocessing/lookAtBadTRs.sh
@@ -10,15 +10,12 @@ OFFSET="$3"
 
 REFERENCE=/projectnb/vntrseek/share/GRCh38/GRCh38.fa
 
-sort -n -k 5 $TR_LIST_FILE > tmp
-mv tmp $TR_LIST_FILE
+# this will produce output as ID chrom start end ... flank_left flank_right array
 
-# this will produce output as ID chrom start end whatever flank_left flank_right TR
-
-echo "reading " $TR_LIST_FILE 
-rm test
+#echo "reading " $TR_LIST_FILE 
+> test
 line=0
-cat $TR_LIST_FILE | while read id chrom start end size missing extra missing_normal extra_normal gma_score indis multi
+cat $TR_LIST_FILE | while read id chrom start end size missing extra gma_score missing_normal extra_normal coverage indis multi
 do
    if [ "$line" -eq "0" ]; then
       line=1
@@ -29,7 +26,6 @@ do
       echo $id$'\t'$chrom:$start-$end$'\t'$size$'\t'$gma_score$'\t'$indis$'\t'$multi$'\t'$LEFT $RIGHT $ARRAY >> test
    fi
 done
-rm tmp.sh
 
-awk '{printf $1 "\t" $2 "\t" $3 "\t" substr(100*$4,1,index(100*$4,".")-1) "." substr(100*$4,index(100*$4,".")+1,3) "%\t" ($5 ? "I" : "S") "\t" ($6 ? "-" : "M") "\t" ($4-$3+1) "\t"  $7 " " $8 " " $9}' test | sort -f -k 5,5 -k 6,6 | uniq -f 5 -i -D > unmarked.indis.BADHOMBRE.txt
+awk '{print $1 "\t" $2 "\t" $3 "\t" substr(100*$4,1,index(100*$4,".")-1) "." substr(100*$4,index(100*$4,".")+1,3) "%\t" ($5 ? "I" : "S") "\t" ($6 ? "-" : "M") "\t"  $7 " " $8 " " $9 }' test | sort -f -k 3,3 -k 7,7 | uniq -f 6 -i -D > ${TR_LIST_FILE%.*}.repeats 
 rm test
